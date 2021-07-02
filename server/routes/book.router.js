6d994eee7b03a6b4f4245989071a0382a574ccb6
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-02 17:24:30 -0500

    Installed Dependancies, Set Up Book Router

---
+++ server/routes/book.router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+   
+})
+
+/**
+ * POST routes go here
+ */
+router.post('/', async (req, res) => {
+    
+});
+
+/**
+ * DELETE routes go here
+ */
+router.delete('/:id', (req, res) => {
+    
+});
+
+/**
+ * PUT routes go here
+ */
+ router.delete('/:id', (req, res) => {
+    
+});
+
+module.exports = router;
